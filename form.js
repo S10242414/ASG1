77bed138a91a519ef0afb3b32f91cd3c0b8416df
Keene Tan <115938171+S10242414@users.noreply.github.com>
2022-12-05 00:05:17 +0800

    Add files via upload

---
+++ form.js
@@ -0,0 +1,28 @@
+document.querySelector("#show-login").addEventListener("click",function(){
+    document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
+    document.querySelector(".popup").classList.remove("active");
+});
+
+
+
+
+
+
+
+
+
+
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  if (window.scrollY >= 800) {
+    goTopBtn.classList.add("active");
+  } else {
+    goTopBtn.classList.remove("active");
+  }
+
+});
