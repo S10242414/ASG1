9b93d6ac30f80afcebd65edc32d8df980a14e76d
Keene Tan <115938171+S10242414@users.noreply.github.com>
2022-12-06 02:55:36 +0800

    Update form.js

--- form.js
+++ form.js
@@ -6,23 +6,26 @@ document.querySelector(".popup .close-btn").addEventListener("click",function(){
 });
 
 
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top")
 
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
 
-
-
-
-
-
-
-
-const goTopBtn = document.querySelector("[data-go-top]");
-
-window.addEventListener("scroll", function () {
-
-  if (window.scrollY >= 800) {
-    goTopBtn.classList.add("active");
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden")
   } else {
-    goTopBtn.classList.remove("active");
+    backToTopButton.classList.add("hidden")
   }
+})
 
-});
+
+
+const goToTop = () => {
+  document.body.scrollIntoView({
+    behavior: "smooth",
+  });
+};
+backToTopButton.addEventListener("click", goToTop)
